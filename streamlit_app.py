59cfe94aa9b5e19e75172078acd76afd44e66ed2
Jonathan Camp <github@jonathan.camp>
2021-04-23 13:47:30 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ import requests
 ctx = st.report_thread.get_report_ctx()
 session_info = Server.get_current()._get_session_info(ctx.session_id)
 
-st.write(session_info.ws.request.headers)
-
 cookie = SimpleCookie(session_info.ws.request.headers.get("Cookie")).get(
     "streamlit_token"
 )
@@ -19,3 +17,9 @@ if not cookie:
 st.write(cookie.value)
 
 st.json(requests.get("https://share.streamlit.io/api/v1/user", cookies={"streamlit_token": cookie.value}).json())
+
+st.markdown("""
+<script>
+alert("hello");
+</script>
+""", unsafe_allow_html=True)
