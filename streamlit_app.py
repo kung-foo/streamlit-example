ef456c123f5ff5e4f156a7c219771af26f71b7c7
Jonathan Camp <github@jonathan.camp>
2021-04-25 11:28:58 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,11 @@ import streamlit.components.v1 as components
 
 
 components.html("""
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script
+			  src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
+
 <script>
 console.log(document.cookie);
 
