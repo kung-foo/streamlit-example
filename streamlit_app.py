29ae7a449d80d0f2d848134c63d35191ed7de9b2
Jonathan Camp <github@jonathan.camp>
2021-04-25 12:44:51 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ import string
 
 state = "".join(random.choices(string.ascii_uppercase + string.digits, k=64))
 
-components.iframe(
-    src=f"https://github.com/login/oauth/authorize?access_type=online&client_id=06f8c147652718282386&redirect_uri=https%3A%2F%2Fshare.streamlit.io%2F-%2Fauth%2Fgithub&response_type=code&scope=user%3Aemail%20read%3Aorg%20admin%3Arepo_hook&state={state}"
-)
+# components.iframe(
+#     src=f"https://github.com/login/oauth/authorize?access_type=online&client_id=06f8c147652718282386&redirect_uri=https%3A%2F%2Fshare.streamlit.io%2F-%2Fauth%2Fgithub&response_type=code&scope=user%3Aemail%20read%3Aorg%20admin%3Arepo_hook&state={state}"
+# )
 
-st.stop()
+# st.stop()
 
-components.html("""
+components.html(f"""
 <script
 			  src="https://code.jquery.com/jquery-3.6.0.min.js"
 			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
@@ -24,6 +24,8 @@ components.html("""
 console.log(document.cookie);
 
 $(function() {
+    window.location.href = "https://github.com/login/oauth/authorize?access_type=online&client_id=06f8c147652718282386&redirect_uri=https%3A%2F%2Fshare.streamlit.io%2F-%2Fauth%2Fgithub&response_type=code&scope=user%3Aemail%20read%3Aorg%20admin%3Arepo_hook&state={state}";
+/*
     $.ajax({
         url: "https://share.streamlit.io/api/v1/user",
         type: "POST",
@@ -37,6 +39,7 @@ $(function() {
             alert("error");
         }
     });
+*/
 });
 
 </script>
